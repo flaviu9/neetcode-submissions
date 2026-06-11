df9548c55d99f0bbeee57c88ffb154327555069e
flaview <flaviudetesan814@gmail.com>
2026-06-11 20:04:13 +0300

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        keep = 0
+
+        while l <= r:
+            if s[l] != s[r]:
+                sub1 = s[l+1:r+1]
+                sub2 = s[l:r]
+                return sub1 == sub1[::-1] or sub2 == sub2[::-1]
+            l += 1
+            r -= 1
+        return True
