d3089baaedf1aca80b0cd8277e53fc05199c1046
flaview <flaviudetesan814@gmail.com>
2026-06-29 16:57:20 +0300

    Add: non-cyclical-number - submission-4

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def sumDigits(n):
+            sum = 0
+            while n:
+                sum += (n%10) ** 2
+                n //= 10
+            return sum
+        hash = {}
+        while True:
+            result = sumDigits(n)
+            if result == 1:
+                return True
+            elif result in hash:
+                hash[result] = 1
+            n = sum
+        return False
