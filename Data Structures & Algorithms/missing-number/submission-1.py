6275f26fc88e1a08701f8cc924abbf6f927f0d5d
flaview <flaviudetesan814@gmail.com>
2026-06-30 19:38:06 +0300

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        sum = 0
+        n = len(nums)
+        realSum = (n*(n+1))/2
+        for elem in range(len(nums)):
+            sum += nums[elem]
+            return int(realSum - sum)
+        return 0
