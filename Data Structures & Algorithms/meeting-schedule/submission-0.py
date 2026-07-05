4856207ca8f67426cbbe82302fecddf7cd3c6e8a
flaview <flaviudetesan814@gmail.com>
2026-07-05 18:06:28 +0300

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,14 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        for i in range(len(intervals)-2):
+            if intervals[i].start > intervals[i+1].start or intervals[i].end > intervals[i+1].start:
+                return False
+        return True
